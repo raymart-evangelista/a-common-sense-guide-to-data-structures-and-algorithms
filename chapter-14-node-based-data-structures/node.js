6564c18a8ab3b6ad4e9609ef38a6622dbe3ee573
raymart-evangelista <raymart.s.evangelista@gmail.com>
2023-08-29 22:24:40 -0700

    add chapter' '

---
+++ chapter-14-node-based-data-structures/node.js
@@ -0,0 +1,87 @@
+class Node {
+  constructor(data) {
+    this.data = data
+    this.next_node = null
+  }
+}
+
+// usage
+node1 = new Node("once")
+node2 = new Node("upon")
+node3 = new Node("a")
+node4 = new Node("time")
+
+node1.next_node = node2
+node2.next_node = node3
+// node3.next_node(node4) this doesn't work because next_node isn't a function
+node3.next_node = node4
+
+// console.log(node3.next_node)
+
+
+class LinkedList {
+  constructor(first_node) {
+    this.first_node = first_node
+  }
+
+  read_data(index) {
+    let current_node = this.first_node
+    for (let current_index = 0; current_index < index; current_index++) {
+      current_node = current_node.next_node
+    }
+    return current_node.data
+  }
+
+  search_data(value) {
+    let current_node = this.first_node
+    let found = false
+    let index = 0
+
+    while (found == false) {
+      if (current_node == null) {
+        index = null
+        break;
+      }
+
+      if (current_node.data == value) {
+        found = true
+      } else {
+        index++
+        current_node = current_node.next_node
+      }
+    }
+    return index
+  }
+
+  insert_at_index(value, index) {
+    let new_node = new Node(value)
+
+    
+    // case when inserting at start
+    if (index == 0) {
+      // old first_node is now the second node
+      new_node.next_node = this.first_node
+      // reestablish what the new first_node in the linked list is
+      this.first_node = new_node
+      return
+    }
+
+    // case when inserting at middle + end
+    let previous_node = this.first_node
+    for (let current_index = 0; current_index < index - 1; current_index++) {
+      previous_node = previous_node.next_node
+    }
+    // new_node's next node will be previous_node's old next_node
+    new_node.next_node = previous_node.next_node
+    // previous_node.next_node will now point to new_node
+    previous_node.next_node = new_node
+    
+  }
+}
+
+const list = new LinkedList(node1)
+// console.log(list.read_data(4))
+// console.log(list.search_data('tisme'))
+console.log(list.read_data(0))
+list.insert_at_index(3, 0)
+console.log(list.read_data(0))
