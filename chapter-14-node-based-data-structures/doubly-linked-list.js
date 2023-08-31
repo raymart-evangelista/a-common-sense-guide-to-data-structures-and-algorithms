375465275f68dde55eaf9fbe5d0de0f19f502e6b
raymart-evangelista <raymart.s.evangelista@gmail.com>
2023-08-30 23:12:06 -0700

    add printing forward + backward

--- chapter-14-node-based-data-structures/doubly-linked-list.js
+++ chapter-14-node-based-data-structures/doubly-linked-list.js
@@ -11,4 +11,46 @@ class DoublyLinkedList {
     this.first_node = first_node
     this.last_node = last_node
   }
+
+  insert_at_end(value) {
+    let new_node = new Node(value)
+    // last node's next_node is the new_node
+    this.last_node.next_node = new_node
+    // the new_node's previous_node will be the current last_node
+    new_node.previous_node = this.last_node
+    // the new_node becomes the last_node
+    this.last_node = new_node
+  }
+
+  print_forward() {
+    let current_node = this.first_node
+    while (current_node) {
+      console.log(current_node.data)
+      current_node = current_node.next_node
+    }
+  }
+  print_backward() {
+    let current_node = this.last_node
+    while(current_node) {
+      console.log(current_node.data)
+      current_node = current_node.previous_node
+    }
+  }
 }
+
+let node1 = new Node('once')
+let node2 = new Node('upon')
+let node3 = new Node('a')
+let node4 = new Node('time')
+
+node1.next_node = node2
+node2.next_node = node3
+node3.next_node = node4
+
+node4.previous_node = node3
+node3.previous_node = node2
+node2.previous_node = node1
+
+let doubly = new DoublyLinkedList(node1, node4)
+// doubly.print_forward()
+doubly.print_backward()
