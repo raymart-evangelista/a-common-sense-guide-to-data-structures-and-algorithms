dfee573ab90f5d8a6efd4ebd403f1d34a18b61f3
raymart-evangelista <raymart.s.evangelista@gmail.com>
2023-09-03 22:32:42 -0700

    create BST class with search function

--- chapter-15-speeding-up-all-the-things-with-binary-search-trees/tree-node.js
+++ chapter-15-speeding-up-all-the-things-with-binary-search-trees/tree-node.js
@@ -9,3 +9,22 @@ class TreeNode {
 node1 = new TreeNode(25)
 node2 = new TreeNode(75)
 root = new TreeNode(50, node1, node2)
+
+class BinarySearchTree {
+  constructor(root=null) {
+    this.root = root
+  }
+  search(searchValue, currentNode=this.root) {
+    if (currentNode === null || currentNode.value === searchValue) {
+      return currentNode
+    }
+
+    if (currentNode.value < searchValue) {
+      return this.search(searchValue, currentNode.rightChild)
+    }
+
+    if (currentNode.value > searchValue) {
+      return this.search(searchValue, currentNode.leftChild)
+    }
+  }
+}
