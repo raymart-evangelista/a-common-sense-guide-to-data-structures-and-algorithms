211e7a7b0feb063880b6f6ef6d50a5189363cd3a
raymart-evangelista <raymart.s.evangelista@gmail.com>
2023-09-21 21:45:26 -0700

    add cities

--- chapter-18-connecting-everything-with-graphs/dijkstra.js
+++ chapter-18-connecting-everything-with-graphs/dijkstra.js
@@ -8,3 +8,19 @@ class City {
     this.routes[city] = price
   }
 }
+
+
+
+let atlanta = new City("Atlanta")
+let boston = new City("Boston")
+let chicago = new City("Chicago")
+let denver = new City("Denver")
+let elPaso = new City("El Paso")
+
+atlanta.addRoute(boston, 100)
+atlanta.addRoute(denver, 160)
+boston.addRoute(chicago, 120)
+boston.addRoute(denver, 180)
+chicago.addRoute(elPaso, 80)
+denver.addRoute(chicago, 40)
+denver.addRoute(elPaso, 140)
