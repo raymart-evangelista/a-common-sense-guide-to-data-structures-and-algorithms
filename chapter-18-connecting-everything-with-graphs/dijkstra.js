c654fafe50fd32ec0ef32d90bd0f625de3c87490
raymart-evangelista <raymart.s.evangelista@gmail.com>
2023-09-24 22:10:13 -0700

    console.log return statement from dijkstra

--- chapter-18-connecting-everything-with-graphs/dijkstra.js
+++ chapter-18-connecting-everything-with-graphs/dijkstra.js
@@ -26,6 +26,7 @@ boston.addRoute(denver, 180)
 chicago.addRoute(elPaso, 80)
 denver.addRoute(chicago, 40)
 denver.addRoute(elPaso, 140)
+elPaso.addRoute(boston, 100)
 
 let citiesByName = {
   "Atlanta": atlanta,
@@ -49,9 +50,9 @@ export function dijkstraShortestPath(startingCity, finalDestination, cities=citi
   while (currentCity) {
     visitedCities[currentCity.name] = true
     // remove that city from the unvisited cities
-    console.log(unvisitedCities)
-    console.log(typeof(currentCity.name))
-    console.log(unvisitedCities.includes(currentCity.name))
+    // console.log(unvisitedCities)
+    // console.log(typeof(currentCity.name))
+    // console.log(unvisitedCities.includes(currentCity.name))
     if (unvisitedCities.includes(currentCity.name)) {
       let index = unvisitedCities.indexOf(currentCity.name)
       unvisitedCities.splice(index, 1)
@@ -63,31 +64,31 @@ export function dijkstraShortestPath(startingCity, finalDestination, cities=citi
       // if discovered a new city, add it to list of unvisited cities
       if (!visitedCities[adjacentCity] && !unvisitedCities.includes(adjacentCity)) {
         unvisitedCities.push(adjacentCity)
-        console.log(`pushed ${adjacentCity} to unvisitedCities`) 
+        // console.log(`pushed ${adjacentCity} to unvisitedCities`) 
       }
       // calculate price of getting from starting city to adjacent city using current city as second-to-last stop
       let priceThroughCurrentCity = cheapestPricesTable[currentCity.name] + currentCity.routes[adjacentCity]
-      console.log(`priceThroughCurrentCity: ${priceThroughCurrentCity}`)
+      // console.log(`priceThroughCurrentCity: ${priceThroughCurrentCity}`)
 
       // if the price from starting city to adjacent city is the cheapest one we found so far...
       if (!cheapestPricesTable[adjacentCity] || priceThroughCurrentCity < cheapestPricesTable[adjacentCity]) {
         // ...update two tables:
         cheapestPricesTable[adjacentCity] = priceThroughCurrentCity
-        console.log(cheapestPricesTable)
+        // console.log(cheapestPricesTable)
         cheapestPreviousStopoverCityTable[adjacentCity] = currentCity.name
-        console.log(cheapestPreviousStopoverCityTable)
+        // console.log(cheapestPreviousStopoverCityTable)
       }
     }
 
     // visit next unvisited city and choose the one that is cheapest to get to from starting city
     let sortedUnvisitedCities = [...unvisitedCities].sort((cityA, cityB) => cheapestPricesTable[cityA.name] - cheapestPricesTable[cityB.name])
-    console.log(`these are the sortedUnvisitedCities`)
-    console.log(sortedUnvisitedCities)
+    // console.log(`these are the sortedUnvisitedCities`)
+    // console.log(sortedUnvisitedCities)
     let cheapestUnvisitedCityName = sortedUnvisitedCities[0]
 
     currentCity = cities[cheapestUnvisitedCityName]
-    console.log(`the cheapest unvisited city name: ${cheapestUnvisitedCityName}`)
-    console.log(currentCity)
+    // console.log(`the cheapest unvisited city name: ${cheapestUnvisitedCityName}`)
+    // console.log(currentCity)
     
   }
 
@@ -104,4 +105,4 @@ export function dijkstraShortestPath(startingCity, finalDestination, cities=citi
   return shortestPath.reverse()
 }
 
-dijkstraShortestPath(atlanta, elPaso, citiesByName)
+console.log(dijkstraShortestPath(atlanta, elPaso, citiesByName))
